74baf9f19edee19d1e903a4069f98676367a1552
nhachan <chanb2014731@student.ctu.edu.vn>
2024-04-18 19:47:46 +0700

    UserAdmin

--- src/components/user/UserList.vue
+++ src/components/user/UserList.vue
@@ -1,33 +1,60 @@
 <template>
-    <div class="table-responsive">
-        <table class=" table table-bordered ">
-            <thead class="table-success">
-                <tr>
-                    <th scope="coll">Tên người dùng</th>
-                    <th scope="coll">Email</th>
-                    <th scope="coll">Địa chỉ</th>
-                    <th scope="coll">Số điện thoại</th>
-                    <th scope="coll"></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(contact, index) in filteredContacts" :key="contact._id" :class="{ active: index === activeIndex }"
-                    @click="updateActiveIndex(index)">
-                    <td>{{ contact.name }}</td>
-                    <td>{{ contact.email }}</td>
-                    <td>{{ contact.address }}</td>
-                    <td>{{ contact.phone }}</td>
-                    <td>
-                        <div>
-                            <router-link :to="{ name: 'contact.edit', params: { id: contact._id}, }">
-                                <button type="button" class="btn btn-outline-success">Sửa</button>
-                            </router-link>
+    <div id="content">
+        <div class="container-fluid">
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Độc giả</h6>
+                </div>
+                <div class="card-body table-responsive">
+                    <div id="dataTable_wrapper" class="dataTables_wrapper dt-bootstrap4">
+                        <!-- <div class="row">
+                            <div class="col-sm-12 col-md-6">
+                                <div id="dataTable_filter" class="dataTables_filter"><label>Search:<input type="search"
+                                            class="form-control form-control-sm" placeholder=""
+                                            aria-controls="dataTable"></label></div>
+                            </div>
+                        </div> -->
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <table class="table table-bordered dataTable" id="dataTable" width="100%"
+                                    cellspacing="0" role="grid" aria-describedby="dataTable_info" style="width: 100%;">
+                                    <thead>
+                                        <tr>
+                                            <th rowspan="1" colspan="1">Tên người dùng</th>
+                                            <th rowspan="1" colspan="1">Email</th>
+                                            <th rowspan="1" colspan="1">Địa chỉ</th>
+                                            <th rowspan="1" colspan="1">Số điện thoại</th>
+                                            <th rowspan="1" colspan="1"></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr v-for="(contact, index) in filteredContacts" :key="contact._id"
+                                            :class="{ active: index === activeIndex }"
+                                            @click="updateActiveIndex(index)">
+                                            <td>{{ contact.name }}</td>
+                                            <td>{{ contact.email }}</td>
+                                            <td>{{ contact.address }}</td>
+                                            <td>{{ contact.phone }}</td>
+                                            <td>
+                                                <div>
+                                                    <router-link
+                                                        :to="{ name: 'contact.edit', params: { id: contact._id }, }">
+                                                        <button type="button"
+                                                            class="btn btn-outline-success">Sửa</button>
+                                                    </router-link>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
 </template>
 
 <script>
